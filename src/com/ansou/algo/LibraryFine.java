678613c37973c9a77132819608bcb4c8b0cb39fb
simon <simondaguenet81@gmail.com>
2019-05-19 21:55:33 +0200

    Library

---
+++ src/com/ansou/algo/LibraryFine.java
@@ -0,0 +1,19 @@
+package com.ansou.algo;
+
+public class LibraryFine {
+
+    static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        if(y2 < y1) return 10000;
+        if(y2 > y1) return 0;
+
+        if(m2 < m1) return 500 * (m1 - m2);
+        if(m2 > m1) return 0;
+
+        if(d2 < d1) return 15 * (d1 - d2);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(libraryFine(9, 6, 2015, 6, 6, 2015));
+    }
+}
