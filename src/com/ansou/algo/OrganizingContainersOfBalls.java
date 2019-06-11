edd21fd752b5272eace159bdb2560519f19c9aa8
simon <simondaguenet81@gmail.com>
2019-06-11 13:32:55 +0200

    Organizing Containers Of balls

---
+++ src/com/ansou/algo/OrganizingContainersOfBalls.java
@@ -0,0 +1,33 @@
+package com.ansou.algo;
+
+import java.util.Arrays;
+
+public class OrganizingContainersOfBalls {
+
+    static String organizingContainers(int[][] container) {
+        int[] a = new int[container.length];
+        int[] b = new int[container.length];
+
+        for(int i = 0; i < container.length; i++)
+        {
+            for(int j = 0; j < container.length; j++)
+            {
+                int x = container[i][j];
+                a[i] += x;
+                b[j] += x;
+            }
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b) ? "Possible" : "Impossible";
+    }
+
+    public static void main(String[] args) {
+        int[][] container = {
+                {0,2,1},
+                {1,1,1},
+                {2,0,0}
+        };
+        System.out.println(organizingContainers(container));
+    }
+}
