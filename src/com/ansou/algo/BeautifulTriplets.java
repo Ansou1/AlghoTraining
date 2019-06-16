81cb081964146f7ba5c49a9a640a74bca4bc3f2d
simon <simondaguenet81@gmail.com>
2019-06-16 15:37:00 +0200

    Beautiful Triplets

---
+++ src/com/ansou/algo/BeautifulTriplets.java
@@ -0,0 +1,27 @@
+package com.ansou.algo;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BeautifulTriplets {
+
+    static int beautifulTriplets(int d, int[] data) {
+        Integer[] arr = Arrays.stream(data).boxed().toArray(Integer[]::new);
+        Set<Integer> myList = new HashSet<>(Arrays.asList(arr));
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (myList.contains(data[i] + d) && myList.contains(data[i] + (2 * d))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 6, 7, 7, 8, 10, 12, 13, 14, 19};
+        System.out.println(beautifulTriplets(3, arr));
+    }
+}
