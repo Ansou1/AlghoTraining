904695cb0e340bfadadab879661bb360c1589528
simon <simondaguenet81@gmail.com>
2019-07-15 12:38:31 +0200

    FindFirstLastPositionElementSortedArray add iterative binarry search start | end

--- src/com/ansou/algo/FindFirstLastPositionElementSortedArray.java
+++ src/com/ansou/algo/FindFirstLastPositionElementSortedArray.java
@@ -2,6 +2,51 @@ package com.ansou.algo;
 
 public class FindFirstLastPositionElementSortedArray {
 
+    /*
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{binarySearchFirstOccurence(nums, target), binarySearchLastOccurence(nums, target)};
+    }
+
+    private int binarySearchLastOccurence(int[] nums, int target) {
+        int h = nums.length - 1;
+        int l = 0;
+        int res = -1;
+
+        while (l <= h) {
+            int mid = l + (h - l)/2;
+
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                h = mid - 1;
+            } else {
+                res = mid;
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    private int binarySearchFirstOccurence(int[] nums, int target) {
+        int h = nums.length - 1;
+        int l = 0;
+        int res = -1;
+
+        while (l <= h) {
+            int mid = l + (h - l)/2;
+
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                h = mid - 1;
+            } else {
+                res = mid;
+                h = mid - 1;
+            }
+        }
+        return res;
+    } */
+
     public static int[] searchRange(int[] nums, int target) {
         return helper(nums, target, 0, nums.length - 1);
     }
